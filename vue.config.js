19cb6f4772b97a5213ae328ce5d7be2c8af09305
Romulo Tunala <romulotunala@gmail.com>
2021-06-01 16:09:39 -0300

    Change proxy

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,5 @@
 module.exports = {
   devServer: {
-    proxy: {
-      '/googleapis': {
-        target: 'https://maps.googleapis.com/',
-        ws: true,
-        changeOrigin: true,
-        logLevel: 'debug',
-        pathRewrite: {
-          '^/googleapis': ''
-        }
-      }
-    }
+    proxy: 'https://maps.googleapis.com/'
   }
 };
